0768b2b4e5291868585d1039c9c7d89fcb1100d0
Shane MacBride <smacbride@mail.csuchico.edu>
2018-03-14 15:58:53 -0700

    Pa 16 create dns aliases (#3)
    * Added basic route53 record for jenkins
    
    * move domain data to root main.tf
    
    * remove excess spacing

--- modules/jenkins_master/main.tf
+++ modules/jenkins_master/main.tf
@@ -4,6 +4,7 @@
 
 variable "aws_key_pair" {}
 variable "tool_name"    {}
+variable "zone_id"      {}
 
 resource "aws_instance" "jenkins_master" {
   ami               = "ami-4bf3d731"
@@ -14,4 +15,12 @@ resource "aws_instance" "jenkins_master" {
     Project = "hackathon_pipeline"
     Name    = "${var.tool_name}_hackathon"
   }
+}
+
+resource "aws_route53_record" "jenkins" {
+  zone_id           = "${var.zone_id}"
+  name              = "jenkins.fastfeedback.rocks"
+  type              = "A"
+  ttl               = 300
+  records           = ["${aws_instance.jenkins_master.private_ip}"]
 }
