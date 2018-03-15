3d82cc9721456c9716b4672f48374d8eab8b4890
Justin Bankes <jbankes@users.noreply.github.com>
2018-03-14 17:22:57 -0700

    PA-21 added JIRA module with necessary specs (#5)
    * PA-21 added JIRA module with necessary specs
    
    * PA-21 Updated ami

--- main.tf
+++ main.tf
@@ -45,3 +45,10 @@ module "bitbucket" {
   tool_name     = "bitbucket"
   zone_id       = "${data.aws_route53_zone.domain.zone_id}"
 }
+
+module "jira" {
+  source        = "./modules/jira"
+  aws_key_pair  = "${var.aws_key_pair}"
+  tool_name     = "jira"
+  zone_id      = "${data.aws_route53_zone.domain.zone_id}"
+}
