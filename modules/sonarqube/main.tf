c84bed6df9e09377c06c57a9ab645915b5c114e0
shanemacbride <shanemacb@gmail.com>
2018-03-15 11:08:08 -0700

    add security group for ssh

--- modules/sonarqube/main.tf
+++ modules/sonarqube/main.tf
@@ -3,13 +3,15 @@
 #
 
 variable "aws_key_pair" {}
-variable "tool_name"    {}
-variable "zone_id"      {}
+variable "tool_name" {}
+variable "zone_id" {}
+variable "ssh_sg" {}
 
 resource "aws_instance" "sonarqube" {
-  ami               = "ami-1853ac65"
-  instance_type     = "t2.large"
-  key_name          = "${var.aws_key_pair}"
+  ami             = "ami-1853ac65"
+  instance_type   = "t2.large"
+  key_name        = "${var.aws_key_pair}"
+  security_groups = ["${var.ssh_sg}"]
 
   root_block_device {
     volume_type = "gp2"
@@ -21,12 +23,11 @@ resource "aws_instance" "sonarqube" {
     Name    = "${var.tool_name}_hackathon"
   }
 }
+
 resource "aws_route53_record" "sonarqube" {
-  zone_id           = "${var.zone_id}"
-  name              = "sonarqube.fastfeedback.rocks"
-  type              = "A"
-  ttl               = 300
-  records           = ["${aws_instance.sonarqube.private_ip}"]
-
+  zone_id = "${var.zone_id}"
+  name    = "sonarqube.fastfeedback.rocks"
+  type    = "A"
+  ttl     = 300
+  records = ["${aws_instance.sonarqube.private_ip}"]
 }
-
