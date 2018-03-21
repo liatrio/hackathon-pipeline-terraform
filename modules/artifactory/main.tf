199c732cb4bb1de9a25582165682a8753fd2eb2f
Justin Bankes <jbankes1@gmail.com>
2018-03-21 16:41:32 -0700

    PA-39 Artifactory volume added; Changed AZ to us-east-1a

--- modules/artifactory/main.tf
+++ modules/artifactory/main.tf
@@ -1,5 +1,5 @@
 #
-# Creates Jenkins Infrastructure
+# Creates Artifactory Infrastructure
 #
 
 variable "aws_key_pair" {}
@@ -7,21 +7,37 @@ variable "tool_name" {}
 variable "zone_id" {}
 variable "ssh_sg" {}
 
+data "aws_ebs_volume" "artifactory_volume" {
+  filter {
+    name   = "tag:Name"
+    values = ["artifactory_data"]
+  }
+
+  most_recent = true
+}
+
 resource "aws_instance" "artifactory" {
-  ami             = "ami-1853ac65"
-  instance_type   = "m5.large"
-  key_name        = "${var.aws_key_pair}"
-  security_groups = ["${var.ssh_sg}"]
+  ami               = "ami-1853ac65"
+  instance_type     = "m5.large"
+  key_name          = "${var.aws_key_pair}"
+  security_groups   = ["${var.ssh_sg}"]
+  availability_zone = "us-east-1a"
 
   root_block_device {
     volume_type = "gp2"
-    volume_size = "500"
+    volume_size = "20"
   }
 
   tags {
     Project = "hackathon_pipeline"
     Name    = "${var.tool_name}_hackathon"
   }
+}
+
+resource "aws_volume_attachment" "artifactory_data" {
+  device_name = "/dev/sdh"
+  volume_id   = "${data.aws_ebs_volume.artifactory_volume.volume_id}"
+  instance_id = "${aws_instance.artifactory.id}"
 }
 
 resource "aws_route53_record" "artifactory" {
