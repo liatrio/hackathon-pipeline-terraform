d8a78a3eb5eea614cb82ca014e0ac4846742c75a
Justin Bankes <jbankes1@gmail.com>
2018-03-21 14:11:42 -0700

    PA-39 Jenkins module attaches EBS volumes

--- modules/jenkins_master/main.tf
+++ modules/jenkins_master/main.tf
@@ -8,21 +8,37 @@ variable "zone_id" {}
 variable "ssh_sg" {}
 variable "jenkins_sg" {}
 
+data "aws_ebs_volume" "jenkins_master_volume" {
+  filter {
+    name   = "tag:Name"
+    values = ["jenkins_master_data"]
+  }
+
+  most_recent = true
+}
+
 resource "aws_instance" "jenkins_master" {
-  ami             = "ami-1853ac65"
-  instance_type   = "m5.large"
-  key_name        = "${var.aws_key_pair}"
-  security_groups = ["${var.ssh_sg}", "${var.jenkins_sg}"]
+  ami               = "ami-1853ac65"
+  instance_type     = "m5.large"
+  key_name          = "${var.aws_key_pair}"
+  security_groups   = ["${var.ssh_sg}", "${var.jenkins_sg}"]
+  availability_zone = "us-east-1a"
 
   root_block_device {
     volume_type = "gp2"
-    volume_size = "500"
+    volume_size = "20"
   }
 
   tags {
     Project = "hackathon_pipeline"
     Name    = "${var.tool_name}_hackathon"
   }
+}
+
+resource "aws_volume_attachment" "jenkins_master_data" {
+  device_name = "/dev/sdh"
+  volume_id   = "${data.aws_ebs_volume.jenkins_master_volume.volume_id}"
+  instance_id = "${aws_instance.jenkins_master.id}"
 }
 
 resource "aws_route53_record" "jenkins" {
